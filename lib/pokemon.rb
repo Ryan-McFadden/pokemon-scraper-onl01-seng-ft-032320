c51f9c565e1c1af448048a80e1004d2915a5b5c9
Ryan McFadden <ryanmcfadden123@gmail.com>
2020-04-26 20:49:31 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,25 @@
 class Pokemon
+  attr_accessor :id, :name, :type, :db
+  
+  def initialize(hash)
+    hash.each do |key, value|
+      self.send(("#{key}="), value)
+    end
+    self.id ||= nil
+  end
+  
+  def self.save(name, type, db)
+    
+    sql = <<-SQL
+      INSERT INTO pokemon (name, type)
+      VALUES (?, ?)
+      SQL
+      
+    DB[:conn].execute(sql, name, type)
+  end
+  
+  def self.find(id)
+    
+  end
+  
 end
