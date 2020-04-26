853cd5eb0fb7a8d64cfc79db2c14cf4f88c08ce0
Ryan McFadden <ryanmcfadden123@gmail.com>
2020-04-26 21:08:54 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -25,14 +25,8 @@ class Pokemon
       WHERE id = ?
       SQL
       
-    pokemon = db.execute(sql, id)
-    
-    new_pokemon = self.new(pokemon)
-    new_pokemon.id = pokemon[0][0]
-    new_pokemon.name = pokemon[0][1]
-    new_pokemon.type = pokemon[0][2]
-    
-    new_pokemon
+    pokemon = db.execute(sql, [id]).flatten
+    Pokemon.new(id, pokemon[1], pokemon[2], pokemon[3], db )
   end
   
 end
